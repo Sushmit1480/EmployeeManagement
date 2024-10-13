cc6035ea23eb685a4bfc864558ea5fc0fcfa00a0
Sushmit1408 <sushmitpatil1@gmail.com>
2024-10-13 14:30:28 +0530

    added code elated to show all employee details

---
+++ src/app/entitites/employee.ts
@@ -0,0 +1,9 @@
+export class Employee {
+  id?: number;
+  firstName?: string;
+  lastName?: string;
+  mobile?: string;
+  email?: string;
+  salary?: number;
+  photo?: string;
+}
---
+++ src/app/services/employees.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Employee } from '../entitites/employee';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EmployeesService {
+  private url = 'http://localhost:8080/api/employees';
+
+  constructor(private httpClient: HttpClient) {}
+
+  getAllEmployee(): Observable<Employee[]> {
+    return this.httpClient.get<Employee[]>(`${this.url}`);
+  }
+}
